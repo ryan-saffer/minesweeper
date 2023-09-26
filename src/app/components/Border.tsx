b8e46aba29098a15b01036f88f0d5a39b5c0c490
Ryan Saffer <ryansaffer@gmail.com>
2023-09-26 18:52:56 +1000

    create border

---
+++ src/app/components/Border.tsx
@@ -0,0 +1,36 @@
+import clsx from "clsx"
+
+export default function Border({
+  type,
+}: {
+  type:
+    | "left"
+    | "right"
+    | "top"
+    | "bottom"
+    | "top-left"
+    | "top-right"
+    | "bottom-left"
+    | "bottom-right"
+}) {
+  return (
+    <div
+      className={clsx("bg-[#BDBDBD]", {
+        "justify-self-end": type.includes("left"),
+        "self-end": type.includes("top"),
+        "w-5 border-l-white border-l-4 border-r-[#7B7B7B] border-r-4":
+          type === "left" || type === "right",
+        "h-5 border-t-white border-t-4 border-b-[#7B7B7B] border-b-4":
+          type === "top" || type === "bottom",
+        "h-5 w-5 border-t-white border-t-4 border-l-white border-l-4":
+          type === "top-left",
+        "h-5 w-5 border-t-white border-t-4 border-r-[#7B7B7B] border-r-4":
+          type === "top-right",
+        "h-5 w-5 border-b-[#7B7B7B] border-b-4 border-l-white border-l-4":
+          type === "bottom-left",
+        "h-5 w-5 border-b-[#7B7B7B] border-b-4 border-r-[#7B7B7B] border-r-4":
+          type === "bottom-right",
+      })}
+    />
+  )
+}
--- src/app/components/MinesweeperBoard.tsx
+++ src/app/components/MinesweeperBoard.tsx
@@ -8,8 +8,9 @@ import Mine from "./icons/Mine"
 import NumberIcon from "./icons/NumberIcon"
 import IncorrectFlag from "./icons/IncorrectFlag"
 import QuestionMark from "./icons/QuestionMark"
+import Border from "./Border"
 
-const size = 10
+const size = 5
 
 export default function MinesweeperBoard() {
   const [minesweeper, setMinesweeper] = useState(
@@ -66,52 +67,74 @@ export default function MinesweeperBoard() {
     }
   }
 
+  const renderTopBottomBorder = (type: "top" | "bottom") => {
+    const result = []
+    result.push(<Border type={`${type}-left`} />)
+    for (const _ of Array(size).keys()) {
+      result.push(<Border type={type} />)
+    }
+    result.push(<Border type={`${type}-right`} />)
+    return result
+  }
+
   return (
-    <>
+    <div className="flex flex-col justify-center items-center">
       <h1>{gameState.secondsPlayed}</h1>
-      <div className="p-10">
+      <div
+        className={`p-10 w-fit grid`}
+        style={{
+          gridTemplateColumns: `repeat(${size + 2}, minmax(0, 1fr))`,
+          gridTemplateRows: `minmax(0, 0.5fr) repeat(${size}, minmax(0, 1fr)) minmax(0, 0.5fr)`,
+        }}
+      >
+        {renderTopBottomBorder("top")}
         {gameState.board.map((row, rowIdx) => (
-          <div key={rowIdx} className="flex justify-center">
+          <>
             {row.map((cell, colIdx) => (
-              <div
-                key={colIdx}
-                className={clsx(
-                  "bg-[#BDBDBD] text-center font-bold text-3xl aspect-square w-10 h-10 flex justify-center items-center border border-[#848484] cursor-pointer relative",
-                  {
-                    "border-t-white border-t-4 border-l-white border-l-4 border-b-[#7B7B7B] border-b-4 border-r-[#7B7B7B] border-r-4":
-                      !cell.revealed,
-                    "border-t-[#7B7B7B] border-t border-b-[#7B7B7B] border-b":
-                      cell.revealed,
-                    "bg-[#fc0303]": cell.isLosingMine,
-                  }
-                )}
-                onClick={(e) => handleClick(e, rowIdx, colIdx)}
-                onContextMenu={(e) => handleClick(e, rowIdx, colIdx)}
-              >
-                {renderIcon(
-                  cell.revealed,
-                  cell.hasFlag,
-                  cell.hasQuestionMark,
-                  cell.value
-                )}
-              </div>
+              <>
+                {colIdx === 0 && <Border type="left" />}
+                <div
+                  key={colIdx}
+                  className={clsx(
+                    "bg-[#BDBDBD] text-center font-bold text-3xl aspect-square w-10 h-10 flex justify-center items-center border border-[#848484] cursor-pointer relative",
+                    {
+                      "border-t-white border-t-4 border-l-white border-l-4 border-b-[#7B7B7B] border-b-4 border-r-[#7B7B7B] border-r-4":
+                        !cell.revealed,
+                      "border-t-[#7B7B7B] border-t border-b-[#7B7B7B] border-b":
+                        cell.revealed,
+                      "bg-[#fc0303]": cell.isLosingMine,
+                    }
+                  )}
+                  onClick={(e) => handleClick(e, rowIdx, colIdx)}
+                  onContextMenu={(e) => handleClick(e, rowIdx, colIdx)}
+                >
+                  {renderIcon(
+                    cell.revealed,
+                    cell.hasFlag,
+                    cell.hasQuestionMark,
+                    cell.value
+                  )}
+                </div>
+                {colIdx === size - 1 && <Border type="right" />}
+              </>
             ))}
-          </div>
+          </>
         ))}
-        {gameState.result !== "running" && (
-          <div className="flex flex-col justify-center items-center pt-8">
-            <h1 className="text-center text-2xl pb-2">
-              {gameState.result === "lost" ? "You lost!" : "You won!"}
-            </h1>
-            <button
-              className="bg-red-400 w-min px-5 py-2 mt-2 rounded-md"
-              onClick={reset}
-            >
-              Reset
-            </button>
-          </div>
-        )}
+        {renderTopBottomBorder("bottom")}
       </div>
-    </>
+      {gameState.result !== "running" && (
+        <div className="flex flex-col justify-center items-center pt-8">
+          <h1 className="text-center text-2xl pb-2">
+            {gameState.result === "lost" ? "You lost!" : "You won!"}
+          </h1>
+          <button
+            className="bg-red-400 w-min px-5 py-2 mt-2 rounded-md"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        </div>
+      )}
+    </div>
   )
 }
