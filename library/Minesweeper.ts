2a4a373ee5e8e0fdac74c8b455c98074c3ab20d7
Ryan Saffer <ryansaffer@gmail.com>
2023-09-26 15:06:34 +1000

    use rxjs
    this moves decision to rerender from react to the library
    this is cleaner and allows things like the timer to be driven by the library

--- library/Minesweeper.ts
+++ library/Minesweeper.ts
@@ -1,18 +1,49 @@
+import { Subject } from "rxjs"
 import { Board } from "./Board"
+
+type GameState = {
+  minesPlaced: number
+  board: Board["_board"]
+  result: "running" | "won" | "lost"
+  secondsPlayed: number
+}
 
 export class Minesweeper {
   private _board: Board
-  private _result: "running" | "won" | "lost" = "running"
+  private _result: GameState["result"] = "running"
+  private _gameState$ = new Subject<GameState>()
+  private _secondsPlayed = 0
+  private _timeout: NodeJS.Timeout
 
   constructor(boardSize: number, private _mineCount: number) {
     this._board = new Board(boardSize, _mineCount)
+
+    this._timeout = setInterval(() => {
+      this._secondsPlayed++
+      this._emitState()
+    }, 1000)
+  }
+
+  get gameState$() {
+    return this._gameState$.asObservable()
+  }
+
+  get initialState(): GameState {
+    return {
+      minesPlaced: 0,
+      board: this._board.board,
+      result: "running",
+      secondsPlayed: 0,
+    }
   }
 
   reveal(row: number, column: number) {
+    if (this._result !== "running") return
     const result = this._board._reveal(row, column, true)
     if (result === "mine") {
       this._result = "lost"
       this._board.revealAllMines()
+      this._stopClock()
     }
     // check for win
     if (
@@ -21,18 +52,26 @@ export class Minesweeper {
     ) {
       this._result = "won"
       this._board.revealBoard()
+      this._stopClock()
     }
+    this._emitState()
+  }
+
+  private _stopClock() {
+    clearInterval(this._timeout)
   }
 
   toggleFlag(row: number, column: number) {
     this._board.board[row][column].toggleFlag()
+    this._emitState()
   }
 
-  getState() {
-    return {
-      minesPlaced: 5,
+  private _emitState() {
+    this._gameState$.next({
+      minesPlaced: 0,
       board: this._board.board,
       result: this._result,
-    }
+      secondsPlayed: this._secondsPlayed,
+    })
   }
 }
--- src/app/components/MinesweeperBoard.tsx
+++ src/app/components/MinesweeperBoard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { MouseEvent, useState } from "react"
+import { MouseEvent, useEffect, useState } from "react"
 import { Minesweeper } from "@minesweeper"
 import { clsx } from "clsx"
 import Flag from "./icons/Flag"
@@ -15,30 +15,30 @@ export default function MinesweeperBoard() {
   const [minesweeper, setMinesweeper] = useState(
     () => new Minesweeper(size, size * 2)
   )
-  const [gameState, setGameState] = useState(minesweeper.getState())
+  const [gameState, setGameState] = useState(minesweeper.initialState)
 
-  const render = () => {
-    setGameState(minesweeper.getState())
-  }
+  useEffect(() => {
+    const subscription = minesweeper.gameState$.subscribe((state) => {
+      setGameState(state)
+    })
+    return () => subscription.unsubscribe()
+  }, [minesweeper])
 
   const handleClick = (e: MouseEvent, row: number, col: number) => {
     e.preventDefault()
 
-    if (gameState.result === "running") {
-      if (e.type === "click") {
-        minesweeper.reveal(row, col)
-      }
-      if (e.type === "contextmenu") {
-        minesweeper.toggleFlag(row, col)
-      }
-      render()
+    if (e.type === "click") {
+      minesweeper.reveal(row, col)
+    }
+    if (e.type === "contextmenu") {
+      minesweeper.toggleFlag(row, col)
     }
   }
 
   const reset = () => {
     const minesweeper = new Minesweeper(size, size * 2)
     setMinesweeper(minesweeper)
-    setGameState(minesweeper.getState())
+    setGameState(minesweeper.initialState)
   }
 
   const renderIcon = (
@@ -67,48 +67,51 @@ export default function MinesweeperBoard() {
   }
 
   return (
-    <div className="p-10">
-      {gameState.board.map((row, rowIdx) => (
-        <div key={rowIdx} className="flex justify-center">
-          {row.map((cell, colIdx) => (
-            <div
-              key={colIdx}
-              className={clsx(
-                "bg-[#BDBDBD] text-center font-bold text-3xl aspect-square w-10 h-10 flex justify-center items-center border border-[#848484] cursor-pointer relative",
-                {
-                  "border-t-white border-t-4 border-l-white border-l-4 border-b-[#7B7B7B] border-b-4 border-r-[#7B7B7B] border-r-4":
-                    !cell.revealed,
-                  "border-t-[#7B7B7B] border-t border-b-[#7B7B7B] border-b":
-                    cell.revealed,
-                  "bg-[#fc0303]": cell.isLosingMine,
-                }
-              )}
-              onClick={(e) => handleClick(e, rowIdx, colIdx)}
-              onContextMenu={(e) => handleClick(e, rowIdx, colIdx)}
+    <>
+      <h1>{gameState.secondsPlayed}</h1>
+      <div className="p-10">
+        {gameState.board.map((row, rowIdx) => (
+          <div key={rowIdx} className="flex justify-center">
+            {row.map((cell, colIdx) => (
+              <div
+                key={colIdx}
+                className={clsx(
+                  "bg-[#BDBDBD] text-center font-bold text-3xl aspect-square w-10 h-10 flex justify-center items-center border border-[#848484] cursor-pointer relative",
+                  {
+                    "border-t-white border-t-4 border-l-white border-l-4 border-b-[#7B7B7B] border-b-4 border-r-[#7B7B7B] border-r-4":
+                      !cell.revealed,
+                    "border-t-[#7B7B7B] border-t border-b-[#7B7B7B] border-b":
+                      cell.revealed,
+                    "bg-[#fc0303]": cell.isLosingMine,
+                  }
+                )}
+                onClick={(e) => handleClick(e, rowIdx, colIdx)}
+                onContextMenu={(e) => handleClick(e, rowIdx, colIdx)}
+              >
+                {renderIcon(
+                  cell.revealed,
+                  cell.hasFlag,
+                  cell.hasQuestionMark,
+                  cell.value
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+        {gameState.result !== "running" && (
+          <div className="flex flex-col justify-center items-center pt-8">
+            <h1 className="text-center text-2xl pb-2">
+              {gameState.result === "lost" ? "You lost!" : "You won!"}
+            </h1>
+            <button
+              className="bg-red-400 w-min px-5 py-2 mt-2 rounded-md"
+              onClick={reset}
             >
-              {renderIcon(
-                cell.revealed,
-                cell.hasFlag,
-                cell.hasQuestionMark,
-                cell.value
-              )}
-            </div>
-          ))}
-        </div>
-      ))}
-      {gameState.result !== "running" && (
-        <div className="flex flex-col justify-center items-center pt-8">
-          <h1 className="text-center text-2xl pb-2">
-            {gameState.result === "lost" ? "You lost!" : "You won!"}
-          </h1>
-          <button
-            className="bg-red-400 w-min px-5 py-2 mt-2 rounded-md"
-            onClick={reset}
-          >
-            Reset
-          </button>
-        </div>
-      )}
-    </div>
+              Reset
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
