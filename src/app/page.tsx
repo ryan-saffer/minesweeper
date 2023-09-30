0f3603bcb4e2eec5b6144239915fac6670806588
Ryan Saffer <ryansaffer@gmail.com>
2023-09-30 23:04:14 +1000

    made level selection buttons nicer
    matchd width, added active css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,11 +32,11 @@ function Level({
 }) {
   return (
     <Link
-      className="h-20 p-8 md:p-12 mt-4 flex flex-col justify-center items-center bg-[#BDBDBD] border border-[#848484] cursor-pointer border-t-white border-t-4 border-l-white border-l-4 border-b-[#7B7B7B] border-b-4 border-r-[#7B7B7B] border-r-4"
+      className="h-20 w-40 md:w-60 p-8 md:p-12 mt-4 flex flex-col justify-center items-center bg-[#BDBDBD] border border-[#848484] cursor-pointer active:border-b-white active:border-b-4 active:border-r-white active:border-r-4 active:border-t-[#7B7B7B] active:border-t-4 active:border-l-[#7B7B7B] active:border-l-4 border-t-white border-t-4 border-l-white border-l-4 border-b-[#7B7B7B] border-b-4 border-r-[#7B7B7B] border-r-4"
       href={`/${name.toLowerCase()}`}
     >
       <h3 className="font-serif text-2xl md:text-4xl">{name}</h3>
-      <p className="font-serif text-sm md:text-lg">{`${size}x${size} - ${mines} mines`}</p>
+      <p className="font-serif text-sm md:text-lg w-max">{`${size}x${size} - ${mines} mines`}</p>
     </Link>
   )
 }
